27995a2f20ed22d80e36425afe1cfd7771c39c30
Thomas Beckley <thomas@tbeckley.com>
2020-07-01 16:13:19 -0400

    Add default state

---
+++ prezcendo/src/constants.js
@@ -0,0 +1,23 @@
+export const GENRES = {
+    JAZZ: 'jazz',
+    ROCK: 'rock',
+    BLUES: 'blues',
+    POP: 'pop'
+};
+
+export const scaleNotes = {
+    A: 0,
+    B: 1,
+    C: 2,
+    D: 3,
+    E: 4,
+    F: 5,
+    G: 6,
+};
+
+export const scaleTypes = {
+    FLAT: 0,
+    NATRUAL: 1,
+    SHARP: 2
+};
+
--- prezcendo/src/store/store.js
+++ prezcendo/src/store/store.js
@@ -1,4 +1,38 @@
 import reducer from './reducer';
 import { createStore } from 'redux';
+import { GENRES, scaleNotes, scaleTypes } from '../constants';
 
-export default createStore(reducer);
+const initialState = {
+    blocks: [],
+    bridges: [
+        {
+            id: 0,
+            startBlockID: 0,
+            endBlockID: 0,
+            currentParameters: {
+                happySad:  0.5,
+                simpleComplex: 0.5,
+                duration: 5 // seconds
+            },
+            scale: [scaleNotes.C, scaleTypes.NATRUAL],
+            previousBars: [], // TBD
+            nextBars: [], // TBD
+            revisions: [
+                /*
+                {
+                    revisonID: 0,
+                    MIDI: [],
+                    rating: 
+                }
+                */
+            ]
+        }
+    ],
+    songSettings: {
+        bpm: 60,
+        timeSignature: [4, 4],
+        genre: GENRES.JAZZ
+    }
+};
+
+export default createStore(reducer, initialState);

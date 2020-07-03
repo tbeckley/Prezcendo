1f4f3cc5a60bf1506d7267ff48c439de530558bb
Azymic <amylu2009@gmail.com>
2020-07-03 00:07:56 -0400

    remove func from main. create text func in main for fitness

--- GA/main.py
+++ GA/main.py
@@ -1,31 +1,22 @@
 from fitnessFunc import *
-import random #for generating random numbers
 
-def initBridge(chordProgress, musicPrev, musicNext, scale):
-    bridge = [[0]*2]*16*8
+#to reduce merge conflicts
+def ALtests():
+    rows, cols = (2, 16*8) #4/4 time for this test case, melody and chord progression as rows
+    bridge = [[0]*cols]*rows
 
-    for row in bridge:
-        print(row)
+    #jazz = fitnessFunc("Jazz")
+    #jazz.printBridge(bridge)
 
-def composeBar(chord, scale):
-    bar = [[0]*2]*16
+    print(genScale(0))
 
 
-def genScale(tonic):
-    #there are 12 notes in total
-    #this is the hexatonic scale (blues)
-    #could potentially use heptatonic scale for more "major" tone in future
-    scale = [tonic, tonic+3, tonic+5, tonic+6, tonic+7, tonic+10, tonic+12]
-    return scale
 
 
 msg = "Hello World. Python is running on your computer."
 print(msg)
 
-rows, cols = (2, 16*8) #4/4 time for this test case, melody and chord progression as rows
-bridge = [[0]*cols]*rows
+ALtests()
 
-jazz = fitnessFunc("Jazz")
-jazz.printBridge(bridge)
 
-print(genScale(0))
+

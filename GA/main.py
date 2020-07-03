82fdb73fb1ba8ef6be087a2d7a9ee482a2484e57
Azymic <amylu2009@gmail.com>
2020-07-03 17:51:18 -0400

    implement evolution function which generates children for a number of specified generations

--- GA/main.py
+++ GA/main.py
@@ -1,5 +1,6 @@
 from fitnessFunc import *
 from DNA import *
+import random
 
 #to reduce merge conflicts
 def ALtests():
@@ -67,13 +68,92 @@ def DNAFitnessTest():
     fitness = jazz.fitnessCalc(child.gene, 0)
     print("FITNESS " + str(fitness))
 
+#credit to https://stackoverflow.com/questions/15036009/suppose-i-have-three-weighted-choices-how-do-i-randomly-select-one
+def weighted_choice(weights):
+    choice = random.random() * sum(weights)
+    for i, w in enumerate(weights):
+        choice -= w
+        if choice < 0:
+            #print("WEIGHT: " + str(i))
+            return i
+
+    #print("WEIGHT: " + str(i))
+    return i
+
+def evolution(gen, childAmt, tonic):
+    #init first population (from midi/tyler)
+    gene1 = [[0, -2, -2, -2, -2, 0, -1, 5, -2, -2, -2, 7, -2, -2, -2, -2, 
+        0, -2, -2, -2, -2, 0, -1, 5, -2, -2, -2, 7, -2, -2, -2, -2, 
+        0, -2, -2, -2, -2, 0, -1, 5, -2, -2, -2, 7, -2, -2, -2, -2, 
+        0, -2, -2, -2, -2, 0, -1, 5, -2, -2, -2, 7, -2, -2, -2, -2,
+        0, -2, -2, -2, -2, 0, -1, 5, -2, -2, -2, 7, -2, -2, -2, -2, 
+        0, -2, -2, -2, -2, 0, -1, 5, -2, -2, -2, 7, -2, -2, -2, -2, 
+        0, -2, -2, -2, -2, 0, -1, 5, -2, -2, -2, 7, -2, -2, -2, -2, 
+        0, -2, -2, -2, -2, 0, -1, 5, -2, -2, -2, 7, -2, -2, -2, -2],
+        
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    gene2 = []
+
+    DNA1 = DNA(4, 4, 2, gene1)
+    DNA2 = DNA(4, 4, 2)
+
+    children = []
+
+    jazz = fitnessFunc("Jazz")
+
+    for i in range(childAmt):
+        children.append(DNA1.crossover(DNA2))
+
+    for g in range(gen):
+        fitness = []
+
+        for val in children:
+            #print(val.gene)
+            fitness.append(jazz.fitnessCalc(val.gene, tonic))
+        #print(fitness)
+
+        newChildren = []
+        for i in range(childAmt):
+            index1 = int(weighted_choice(fitness))
+            index2 = int(weighted_choice(fitness))
+
+            while index1 == index2: #don't want asexual reproduction
+                index2 = weighted_choice(fitness)
+
+            parent1 = children[index1]
+            parent2 = children[index2]
+
+            newChildren.append(parent1.crossover(parent2))
+            newChildren[-1].mutate() #mutates last element
+
+        children = newChildren
+
+    print(fitness)
+    for val in children:
+        print(val.gene)
+    
+
+    #generate 5 children
+    #apply fitness to all children
+    #normalize
+    #use to calculate probability of selecting child as parent
+    #generate 5 new children from randomly selected parents (randomly select each time, reroll if it's the same)
 
 msg = "Hello World. Python is running on your computer."
 print(msg)
 
-ALtests()
+evolution(100, 5, 0)
 
-DNAFitnessTest()
+#tests
+#ALtests()
+#DNAFitnessTest()
 
 
 

e90940eefe5110bce582d02847a8a6ed252a2b40
tylerdsilva <tylerdsilva1@gmail.com>
2020-07-04 16:56:03 -0400

    Formatted other code and adding midi-read code

--- GA/DNA.py
+++ GA/DNA.py
@@ -1,6 +1,7 @@
 import math
 import random
 
+
 # class for the DNA
 class DNA:
     # I'm going to assume the bridge will always be 8 bars long
@@ -13,13 +14,13 @@ class DNA:
     # gene: 2d array with dimensions tracks*length
     # fitness: an object's fitness, defaults to 0 but can be calculated using fitness functions
 
-    mutationRate = 0.01 # replace with whatever rate you want
-    highestNote = 24 # assuming lowest note is 0, and we want a 2-octave span
+    mutationRate = 0.01  # replace with whatever rate you want
+    highestNote = 24  # assuming lowest note is 0, and we want a 2-octave span
 
     def __init__(self, numerator, denominator, track, genes=None):
         self.nn = numerator
         self.dd = denominator
-        self.length = int(16*8*numerator/denominator)
+        self.length = int(16 * 8 * numerator / denominator)
         self.tracks = track
         self.fitness = 0
 
@@ -36,16 +37,16 @@ class DNA:
             self.gene = genes
 
             # To Do later: making sure the 2d array that was given matches the size that was given
-            
+
             # if (len(self.gene) > self.tracks):
-                # cut out extra tracks
+            # cut out extra tracks
             # elif (len(self.gene) < self.tracks):
-                # add empty tracks
+            # add empty tracks
             # if (len(self.gene[0]) > self.length):
-                # cut out extra notes
+            # cut out extra notes
             # elif (len(self.gene[0]) < self.length):
-                # add empty notes
-        
+            # add empty notes
+
     def mutate(self):
         for track in range(len(self.gene)):
             for note in range(len(self.gene[track])):
@@ -56,51 +57,44 @@ class DNA:
                 # elif ((surprise >= self.mutationRate)):
                 #     newNote = random.randrange(-2, 24) # random from -2 to 24
                 #     self.gene[track][note] = newNote
-                if ((surprise >= self.mutationRate)):
-                    newNote = random.randrange(-2, 4) # 1/6 chance of being rest or hold, 4/6 chance of being new note
+                if surprise >= self.mutationRate:
+                    newNote = random.randrange(-2, 4)  # 1/6 chance of being rest or hold, 4/6 chance of being new note
                     if (newNote >= 0):
                         newNote = random.randrange(0, 24)
                     self.gene[track][note] = newNote
-        #print("BEFORE FIXUP " + str(self.gene)) 
+        # print("BEFORE FIXUP " + str(self.gene))
         self.fixup()
-        #print("AFTER FIXUP " + str(self.gene))  
+        # print("AFTER FIXUP " + str(self.gene))
 
     # crossover function, picks a random location between bars and splits there
     def crossover(self, partner):
-        location = random.randrange(1, 7) # random split location between 1 and 7
-        firstParent = random.randrange(0, 1) # pick a random parent to be "first"
+        location = random.randrange(1, 7)  # random split location between 1 and 7
+        firstParent = random.randrange(0, 1)  # pick a random parent to be "first"
         child = []
+        chooseTrack = self if firstParent == 0 else partner
+        otherTrack = partner if firstParent == 0 else self
 
-        if (firstParent == 0):
-            #print(len(self.gene))
-            for track in range(len(self.gene)):
-                newTrack = self.gene[track][0:int(self.length*location/8)]
-                #print("NEW TRACK " + str(newTrack))
-                newTrack.extend(partner.gene[track][int(self.length*location/8) : int(self.length)])
-                #print("NEW TRACK AFTER " + str(newTrack))
-                child.append(newTrack)
-        else:
-            for track in range(len(self.gene)):
-                newTrack = partner.gene[track][0:int(self.length*location/8)]
-                #print("NEW TRACK " + str(newTrack))
-                newTrack.extend(self.gene[track][int(self.length*location/8) : int(self.length)])
-                #print("NEW TRACK AFTER " + str(newTrack))
-                child.append(newTrack)
-        
-        #print("CHILD " + str(child))
+        for track in range(len(self.gene)):
+            newTrack = chooseTrack.gene[track][0:int(self.length * location / 8)]
+            # print("NEW TRACK " + str(newTrack))
+            newTrack.extend(otherTrack.gene[track][int(self.length * location / 8): int(self.length)])
+            # print("NEW TRACK AFTER " + str(newTrack))
+            child.append(newTrack)
+
+        # print("CHILD " + str(child))
         childDNA = DNA(self.nn, self.dd, self.tracks, child)
         childDNA.fixup()
-        #print("CHILD " + str(child))
+        # print("CHILD " + str(child))
         return childDNA
 
     # function for removing holds (-2) that don't have a proper start
-    #TODO: still needs to be tested
+    # TODO: still needs to be tested
     def fixup(self):
         for track in range(len(self.gene)):
             for note in range(len(self.gene[track])):
-                if ((self.gene[track][note] == -2) and (note == 0)):
+                if (self.gene[track][note] == -2) and (note == 0):
                     self.gene[track][note] = -1
-                elif ((self.gene[track][note] == -2) and (self.gene[track][note-1] == -1)):
+                elif (self.gene[track][note] == -2) and (self.gene[track][note - 1] == -1):
                     self.gene[track][note] = -1
 
     def setFitness(self, newFitness):
--- GA/fitnessFunc.py
+++ GA/fitnessFunc.py
@@ -1,134 +1,125 @@
-#class for assessing fitness
+# class for assessing fitness
 
 class fitnessFunc:
-    genre = "Jazz" #default for now, will be enum with different genres later 
-    #bridge = [[0]*1]*1
+    genre = "Jazz"  # default for now, will be enum with different genres later
+
+    # bridge = [[0]*1]*1
 
     def __init__(self, genre):
         self.genre = genre
-        #making a deep copy of the bridge
-        #self.bridge = [row[:] for row in bridge] #[:] is slice notation
+        # making a deep copy of the bridge
+        # self.bridge = [row[:] for row in bridge] #[:] is slice notation
 
     def printBridge(self, bridge):
         for row in bridge:
             print(row)
 
     def genJazzScale(self, tonic):
-        #there are 12 notes in total
-        #this is the hexatonic scale (blues)
-        #could potentially use heptatonic scale for more "major" tone in future
-        scale = [tonic, tonic+3, tonic+5, tonic+6, tonic+7, tonic+10, tonic+12]
-        return scale    
-    
-    #note that this assumes 4/4 time, will implement other time signatures in future
+        # there are 12 notes in total
+        # this is the hexatonic scale (blues)
+        # could potentially use heptatonic scale for more "major" tone in future
+        scale = [tonic, tonic + 3, tonic + 5, tonic + 6, tonic + 7, tonic + 10, tonic + 12]
+        return scale
+
+        # note that this assumes 4/4 time, will implement other time signatures in future
+
     def fitnessCalc(self, bridge, tonic):
         melody = bridge[0]
 
-        #print(melody)
+        # print(melody)
 
-        longRest = 0 #neg ten points for rests longer than a half note
-        curRestLen = 0 
+        longRest = 0  # neg ten points for rests longer than a half note
+        curRestLen = 0
 
-        longNote = 0 #neg ten points for notes longer than a whole note
+        longNote = 0  # neg ten points for notes longer than a whole note
         curNoteLen = 0
 
-        #not done
-        uniqueNoteBar = [0, 0, 0, 0, 0, 0, 0, 0] #two points for every bar with at least two different pitches
-        #not done
-        totalNotes = [0, 0, 0, 0, 0, 0, 0, 0] #neg 10 points for bars with over 8 notes
+        # not done
+        uniqueNoteBar = [0, 0, 0, 0, 0, 0, 0, 0]  # two points for every bar with at least two different pitches
+        # not done
+        totalNotes = [0, 0, 0, 0, 0, 0, 0, 0]  # neg 10 points for bars with over 8 notes
 
-        #rules for jazz
+        # rules for jazz
 
         scale = self.genJazzScale(tonic)
-        
-        noteOnScale = 0 #one point per note on the jazz scale
-        int7 = 0 #two points per dominant 7th, max four points
-        int45 = 0 #one point per perfect fifth or perfect fourth, max four points or something
 
-        noteOnDown = 0 #one point for every note started on 2nd or 4th beat
-        #notDone
-        noteDiffBar = 0 #one point for every bar with at least two note lengths
+        noteOnScale = 0  # one point per note on the jazz scale
+        int7 = 0  # two points per dominant 7th, max four points
+        int45 = 0  # one point per perfect fifth or perfect fourth, max four points or something
 
-        dottedQuarter = 0 #two points for every dotted quarter note, max four points
+        noteOnDown = 0  # one point for every note started on 2nd or 4th beat
+        # notDone
+        noteDiffBar = 0  # one point for every bar with at least two note lengths
 
-        restEight = 0 #two points for every eigth rest, max four points
-        restQuarter = 0 #two points for every quarter rest, max four points
+        dottedQuarter = 0  # two points for every dotted quarter note, max four points
+
+        restEight = 0  # two points for every eigth rest, max four points
+        restQuarter = 0  # two points for every quarter rest, max four points
 
         prevNote = None
 
         for index, val in enumerate(melody):
 
-            if (index < len(melody)-1):
-                next = melody[index+1]
+            if index < len(melody) - 1:
+                next = melody[index + 1]
             else:
                 next = None
 
-            if (val == -1): #rest
+            if val == -1:  # rest
                 curRestLen += 1
-                
+
                 if (next is None) or (next != -2):
-                    if (curRestLen == 2):
+                    if curRestLen == 2:
                         restEight += 1
-                    elif (curRestLen == 4):
+                    elif curRestLen == 4:
                         restQuarter += 1
-                    elif (curRestLen == 8):
+                    elif curRestLen == 8:
                         longRest += 1
 
-            if (val == -2): #hold
+            if val == -2:  # hold
                 curNoteLen += 1
 
-                #check for dotted quarter notes
+                # check for dotted quarter notes
                 if (next is None) or (next == -2):
-                    if (curNoteLen == 6):
+                    if curNoteLen == 6:
                         dottedQuarter += 1
-                    elif (curNoteLen == 16):
+                    elif curNoteLen == 16:
                         longNote += 1
 
-                if (prevNote is not None):
+                if prevNote is not None:
                     if (prevNote - val == 1) or (prevNote - val == -11):
                         int7 += 1
                     if (abs(prevNote - val) == 4) or (abs(prevNote - val) == 5):
                         int45 += 1
                 prevNote = val
 
-            if (val >= 0): #new note
+            if val >= 0:  # new note
                 curNoteLen = 1
 
                 for j in scale:
-                    if (val == j):
+                    if val == j:
                         noteOnScale += 1
 
-                if(index%16 == 4) or (index%16 == 12): #each bar has 16 notes
+                if (index % 16 == 4) or (index % 16 == 12):  # each bar has 16 notes
                     noteOnDown += 1
 
-
-            if(next is not None): #reset current length counters
-                if (next == -1):
+            if next is not None:  # reset current length counters
+                if next == -1:
                     curNoteLen = 0
-                elif (next >= 0):
+                elif next >= 0:
                     curRestLen = 0
 
-        #for the rules with bounded points (so we don't end up with a bajillion dotted quarter notes)
+        # for the rules with bounded points (so we don't end up with a bajillion dotted quarter notes)
         dottedQuarter = min(dottedQuarter, 2)
         restEight = min(restEight, 2)
         restQuarter = min(restQuarter, 2)
         int7 = min(int7, 2)
         int45 = min(int45, 2)
 
-        fitness = (longNote + longRest)*(-10) + (noteOnScale + noteOnDown + noteDiffBar)*1 + (dottedQuarter + restEight + restQuarter + int7 + int45)*2
-        if (fitness < 1):
-            fitness = 1 #no negative fitness allowed, not the best fix but will adjust later
+        fitness = (longNote + longRest) * (-10) + (noteOnScale + noteOnDown + noteDiffBar) * 1 + (
+                dottedQuarter + restEight + restQuarter + int7 + int45) * 2
+        if fitness < 1:
+            fitness = 1  # no negative fitness allowed, not the best fix but will adjust later
         return fitness
 
-        #TODO: actually compute and return the fitness value
-
-        
-    
-
-
-
-
-
-
-    
-
+        # TODO: actually compute and return the fitness value
